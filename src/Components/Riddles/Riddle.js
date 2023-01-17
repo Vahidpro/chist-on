3f31a2ce73efe64321c6b054ed5b1551cb0dca51
Vahid Porki <ivahidp@gmail.com>
2023-01-17 17:33:32 +0330

    Add data dynamically from an array

--- src/Components/Riddles/Riddle.js
+++ src/Components/Riddles/Riddle.js
@@ -3,8 +3,9 @@ import classes from "./Riddle.module.css";
 const Riddle = (props) => {
 	return (
 		<div className={classes.container}>
-			<p className={classes.question}>آن چیست که چیستان است؟</p>
-			<p className={classes.answer}>جواب: چیستان! 😂😂😂</p>
+			<p className={classes.question}>{props.question}</p>
+			<p className={classes.answer}>{props.answer}</p>
+			{console.log(props.question)}
 		</div>
 	);
 };
--- src/Components/Riddles/RiddlesList.js
+++ src/Components/Riddles/RiddlesList.js
@@ -1,7 +1,17 @@
 import Riddle from "./Riddle";
 
 const RiddlesList = (props) => {
-	return <Riddle></Riddle>;
+	return (
+		<div>
+			{props.riddles.map((item) => (
+				<Riddle
+					key={item.id}
+					question={item.question}
+					answer={item.answer}
+				></Riddle>
+			))}
+		</div>
+	);
 };
 
 export default RiddlesList;
