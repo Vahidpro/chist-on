cd189a504e36ddbc8dff80ddd677896d2cd45ec3
Vahid Porki <ivahidp@gmail.com>
2023-01-17 18:28:32 +0330

    Show answer with button click

--- src/Components/Riddles/Riddle.js
+++ src/Components/Riddles/Riddle.js
@@ -1,11 +1,24 @@
+import AnimateHeight from "react-animate-height";
 import classes from "./Riddle.module.css";
+import { useState } from "react";
 
 const Riddle = (props) => {
+	const [height, setHeight] = useState(0);
 	return (
 		<div className={classes.container}>
 			<p className={classes.question}>{props.question}</p>
-			<p className={classes.answer}>{props.answer}</p>
-			{console.log(props.question)}
+
+			<button
+				aria-expanded={height !== 0}
+				aria-controls="example-panel"
+				onClick={() => setHeight(height === 0 ? "auto" : 0)}
+				className={classes.btn_answer}
+			>
+				{height === 0 ? "جواب" : "بستن"}
+			</button>
+			<AnimateHeight id="example-panel" duration={500} height={height}>
+				<p className={classes.answer}>{props.answer}</p>
+			</AnimateHeight>
 		</div>
 	);
 };
